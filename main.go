a74f295ef3f304a230dc923226a8b1bc2873d11a
Tom Kadwill <tomkadwill@gmail.com>
2015-11-04 21:22:03 +0000

    Added example POST request and removed hellow world route

--- main.go
+++ main.go
@@ -4,17 +4,33 @@ import (
     "fmt"
     "net/http"
     "os"
+    "bytes"
+    "io/ioutil"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
+    url := "http://requestb.in/sd03fwsd"
+    var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+
+
     fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+    err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
       panic(err)
     }
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
 }
