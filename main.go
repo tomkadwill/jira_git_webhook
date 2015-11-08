88a3c90b48ad8b76fc5656c99a80d3f2a1b72a49
Tom Kadwill <tomkadwill@gmail.com>
2015-11-08 19:47:03 +0000

    Refactor

--- main.go
+++ main.go
@@ -99,38 +99,9 @@ func hello(res http.ResponseWriter, req *http.Request) {
         fmt.Println("does it match??")
 
         if (match==true && failures==false) {
-          s = []string{"https://api.github.com/repos/tomkadwill/mud/statuses/", commit.Sha}
-          url := strings.Join(s, "")
-          // url := "https://api.github.com/repos/tomkadwill/mud/statuses/fed9d6dc2155cea9fb5bbce3243372194acc9fc4"
-          var jsonStr = []byte(`{"state": "success","target_url": "https://example.com/build/status","description": "The build succeeded!","context": "JIRA/check"}`)
-          req, err = http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-          req.Header.Set("X-Custom-Header", "myvalue")
-          req.Header.Set("Content-Type", "application/json")
-          req.SetBasicAuth(os.Getenv("GITHUB_USERNAME"), os.Getenv("GITHUB_PASSWORD"))
-
-          client := &http.Client{}
-          resp, err := client.Do(req)
-          if err != nil {
-              panic(err)
-          }
-          defer resp.Body.Close()
-
+          setStatus(commit.Sha, "success")
         } else {
-          s = []string{"https://api.github.com/repos/tomkadwill/mud/statuses/", commit.Sha}
-          url := strings.Join(s, "")
-          var jsonStr = []byte(`{"state": "failure","target_url": "https://example.com/build/status","description": "One of more of your stories does not contain a JIRA number","context": "JIRA/check"}`)
-          req, err = http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-          req.Header.Set("X-Custom-Header", "myvalue")
-          req.Header.Set("Content-Type", "application/json")
-          req.SetBasicAuth(os.Getenv("GITHUB_USERNAME"), os.Getenv("GITHUB_PASSWORD"))
-
-          client := &http.Client{}
-          resp, err := client.Do(req)
-          if err != nil {
-              panic(err)
-          }
-          defer resp.Body.Close()
-
+          setStatus(commit.Sha, "failure")
           failures = true
         }
       }
@@ -138,4 +109,21 @@ func hello(res http.ResponseWriter, req *http.Request) {
     } else {
         // Do something
     }
+}
+
+func setStatus(sha string, state string) {
+  s := []string{"https://api.github.com/repos/tomkadwill/mud/statuses/", sha}
+  url := strings.Join(s, "")
+  var jsonStr = []byte(`{"state": "state","target_url": "https://example.com/build/status","description": "One of more of your stories does not contain a JIRA number","context": "JIRA/check"}`)
+  req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+  req.Header.Set("X-Custom-Header", "myvalue")
+  req.Header.Set("Content-Type", "application/json")
+  req.SetBasicAuth(os.Getenv("GITHUB_USERNAME"), os.Getenv("GITHUB_PASSWORD"))
+
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+      panic(err)
+  }
+  defer resp.Body.Close()
 }
